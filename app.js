d762b78172dece0f73084dc91f00070bec9cadaa
Ruslan A <aleyev.rus@gmail.com>
2022-11-21 08:03:06 +0300

    Mind_storm

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const SQUARES_NUMBER = 500
 for (let i=0; i< SQUARES_NUMBER; i++ ) {
     const square = document.createElement('div')
     square.classList.add('square')
+     
+     
 
-    board.append(square)
+     square.addEventListener('mouseover', () => 
+     DoTranspSquare(square))
     
-}
+     board.append(square)
+    }
+     function DoTranspSquare(element) {
+        rgba(element.style.backgroundColor)
+     }
+    
